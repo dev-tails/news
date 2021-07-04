239d3eb542d862925c55a68a6d8c94a5f19001c1
Adam Berg <adam@xyzdigital.com>
2021-07-04 00:30:17 -0700

    Render index template via crow

--- api/api.cpp
+++ api/api.cpp
@@ -5,7 +5,16 @@ int main()
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+        crow::mustache::context ctx;
+        std::vector<crow::json::wvalue> articles(1);
+
+        articles[0]["title"] = "Headline";
+        articles[0]["href"] = "http://google.ca";
+        articles[0]["url"] = "google.ca";
+
+        ctx["articles"] = std::move(articles);
+        auto page = crow::mustache::load("index.html");
+        return page.render(ctx);
     });
 
     app.port(18080).multithreaded().run();
